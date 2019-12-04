fd66bf3f12edd1decdb6efd032a5887e65913ddd
Ratimir <ra0953029364@gmail.com>
2019-12-04 06:21:39 +0200

    Добавьте файлы проекта.

---
+++ 16 tasks/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _16_tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("ВВедите число num1 и num2");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine("num1 больше num 2");
+            }
+            else if (num1 < num2)
+            {
+                Console.WriteLine("num2 больше num 1");
+            }
+            else
+            {
+                Console.WriteLine("num1 равно num 2");
+            }
+        }
+    }
+}
